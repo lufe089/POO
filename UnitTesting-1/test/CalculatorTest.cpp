9ca35071f16893a00f363dda38ba972a42d234db
Luisa Rincon <lufe089@gmail.com>
2021-11-03 12:42:10 -0500

    MOD: Se agregan metodos aprovechando sobrecarga y se muestran para ejecutar pruebas unitarias

--- UnitTesting-1/test/CalculatorTest.cpp
+++ UnitTesting-1/test/CalculatorTest.cpp
@@ -15,7 +15,21 @@ TEST(CalculatorTest, add){   //Arrange section
     result = myCalculator.add(value1, value2);
 
     //Assert section
-    EXPECT_EQ(10,result);
+    EXPECT_EQ(15,result);
+}
+
+TEST(CalculatorTest, addThreeValues){   //Arrange section
+    int value1 = 5;
+    int value2 = 10;
+    int value3 = 15;
+    int result = 0;
+
+    //Act section
+    Calculator myCalculator;
+    result = myCalculator.add(value1, value2, value3);
+
+    //Assert section
+    EXPECT_EQ(15,result);
 }
 
 TEST(CalculatorTest, substractLowerNumber){
